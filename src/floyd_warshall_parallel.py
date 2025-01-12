a2dbdc9fcaf63fe3353a08434f51d95c751c57df
Sofia-Romanenko <soomphik@gmail.com>
2025-01-13 00:05:37 +0300

    Update floyd_warshall_parallel.py

--- src/floyd_warshall_parallel.py
+++ src/floyd_warshall_parallel.py
@@ -2,17 +2,24 @@ from multiprocessing import Pool
 import numpy as np
 
 def update_row(args):
+    """
+    Updates a single row of the distance matrix for a given k.
+    """
     i, k, dist = args
     n = len(dist)
     for j in range(n):
         dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
     return dist[i]
 
-def floyd_warshall_parallel(matrix, workers=4):
+def floyd_warshall_parallel_optimized(matrix, workers=4):
+    """
+    Parallel implementation of the Floyd-Warshall algorithm.
+    """
     n = len(matrix)
     dist = matrix.copy()
     for k in range(n):
+        dist_k = dist[k].copy()  # Extract the k-th row once
         with Pool(workers) as pool:
-            rows = pool.map(update_row, [(i, k, dist) for i in range(n)])
-        dist = np.array(rows)
+            updated_rows = pool.map(update_row, [(i, k, dist) for i in range(n)])
+        dist = np.array(updated_rows)
     return dist
